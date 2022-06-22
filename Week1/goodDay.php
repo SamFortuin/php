d79c3bbfdf37e1e5c540e7676564ffda3a239230
SamFortuin <99059050@mydavinci.nl>
2022-06-22 17:44:15 +0200

    finished goodDay

--- Week1/goodDay.php
+++ Week1/goodDay.php
@@ -1,15 +1,44 @@
-<script>
-        //var today = "Wed Jun 22 2022 14:30:23 GMT+0200 (Central European Summer Time)" //static for testing
-        var today = new Date();
-</script>
-
-<?php
-echo $today = "<script>document.write(today)</script>";
-echo $imageURL = "images/afternoon.png";
-?>
-
+<!DOCTYPE html>
 <style>
-        body{
-            background-image: url(<?php echo $imageURL;?>);
-        }
+    .center {
+        margin-top: 20pc;
+        text-align: center;
+        color: white;
+        font-size: 120px;
+    }
 </style>
+
+<body id="main" style="background-size:cover;">
+<meta http-equiv="refresh" content="1">
+<?php
+    function changeBG($inStr,$inTime){
+        $translate = array(
+            'morning' => 'morgen',
+            'afternoon' => 'middag',
+            'evening' => 'avond',
+            'night' => 'nacht'
+        );
+        $imageURL = 'images/'.$inStr.'.png';
+        echo "<h1 class=center>Goede $translate[$inStr]<br>$inTime</h1>";
+        echo '<script>document.getElementById("main").style.backgroundImage = "url('.$imageURL.')";</script>';
+    }
+    
+    
+    date_default_timezone_set('Europe/Amsterdam');
+    $currentTime = date('H:i:s')."<br>";
+    $switchTime = date('H');
+
+    if ($switchTime >= 0 and $switchTime < 6){
+        changeBG('night',$currentTime);
+    }
+    elseif ($switchTime > 5 and $switchTime < 12){
+        changeBG('morning',$currentTime);
+    }
+    elseif ($switchTime > 11 and $switchTime < 18){
+        changeBG('afternoon',$currentTime);
+    }
+    elseif ($switchTime > 17 and $switchTime <= 23){
+        changeBG('evening',$currentTime);
+    }
+?>
+</body>
